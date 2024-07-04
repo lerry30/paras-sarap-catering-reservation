bfaecfbf6c30b136385ddfb0c7ce92dea9d827b8
Lerry <johnlerryramossamson@gmail.com>
2024-07-04 09:15:12 +0800

    Hero page enhanced

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,11 @@
 
 import CNavbar from '@/components/client/nav/CNavbar';
 import Image from 'next/image';
-import HeroImg5 from '../../public/Hero.png';
+import BgImg from '../../public/bg.jpg';
+//import HeroImg5 from '../../public/Hero.png';
+import Event01 from '../../public/event01.jpg';
+import Event02 from '../../public/event02.jpg';
+import Event03 from '../../public/event03.jpg';
 
 import WeddingImg from '../../public/wedding.jpg';
 import DebutImg from '../../public/debut.jpeg';
@@ -19,47 +23,83 @@ export default function Home() {
         <div className="pt-[var(--nav-height)]">
             <CNavbar />
             <main className="w-full py-4 px-page-x flex flex-col gap-16 md:gap-4">
-                <section className="w-full flex flex-col md:flex-row gap-4 md:h-[calc(100vh-var(--nav-height)-32px)]">
-                    <div className="w-full md:w-1/2 flex flex-col justify-center">
-                        <h2 className="font-headings text-3xl">Welcome to Paras Sarap Catering Reservation</h2>
-                        <h4 className="font-headings text-lg">Your Premier Catering Reservation Solution</h4>
-                        <p className="font-paragraphs text-neutral-700 text-sm">At Paras Sarap Catering Reservation, we streamline catering reservations, eliminating the hassle of coordinating food for your events. From weddings to corporate gatherings, our user-friendly platform and diverse menu options ensure culinary success, allowing you to focus on creating unforgettable moments.</p>
-                        <div className="flex gap-2 mt-4">
-                            <Link href="/about" className="rounded-sm border-[1px] border-neutral-600 p-2">Learn More</Link>
-                            <Link href="/services" className="rounded-sm border-[1px] bg-skin-ten p-2 text-white">Reserve Now</Link>
-                        </div>
-                        <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
+                <section className="relative w-full h-[1100px] md:h-[900px] lg:h-[calc(100vh-var(--nav-height))]"> 
+                    <div className="absolute -top-4 -left-[var(--page-x-padding)] -right-[var(--page-x-padding)] bottom-0 bg-gradient-to-r from-teal-900 via-teal-800 to-[var(--skin-ten)]">
+                        <Image 
+                            src={ BgImg }
+                            alt={ 'Background Image' }
+                            sizes='100%'
+                            style={{
+                                objectFit: 'cover',
+                                transformOrigin: 'center',
+                                position: 'absolute',
+                                mixBlendMode: 'overlay',
+                                zIndex: 2,
+                                opacity: .18,
+                                filter: 'blur(6px)',
+                            }}
+                            priority
+                        />
                     </div>
-                    <div className="w-full md:w-1/2 flex md:justify-end md:mt-14">
-                        <div className="size-full aspect-square md:size-[400px] md:min-w-[400px] rounded-lg shadow-xl">
-                            <Image 
-                                src={ HeroImg5 }
-                                alt={ 'Paras Cater' }
-                                width={ 200 }
-                                height={ 200 }
-                                sizes='100%'
-                                style={{
-                                    width: '100%',
-                                    height: '100%',
-                                    objectFit: 'cover',
-                                    transformOrigin: 'center',
-                                    borderRadius: '20px'
-                                }}
-                                priority
-                            />
+                    <div className="absolute size-full flex flex-col gap-4 lg:flex-row">
+                        <div className="w-full lg:w-1/2 flex flex-col justify-center text-white">
+                            <h2 className="font-headings text-4xl font-bold">Welcome to Paras Sarap Catering Reservation</h2>
+                            <h4 className="font-headings text-lg">Your Premier Catering Reservation Solution</h4>
+                            <p className="font-paragraphs text-neutral-100 text-sm ">At Paras Sarap Catering Reservation, we streamline catering reservations, eliminating the hassle of coordinating food for your events. From weddings to corporate gatherings, our user-friendly platform and diverse menu options ensure culinary success, allowing you to focus on creating unforgettable moments.</p>
+                            <div className="flex gap-2 mt-4">
+                                <Link href="/about" className="rounded-sm border-[1px] border-neutral-600 p-2">Learn More</Link>
+                                <Link href="/services" className="rounded-sm border-[1px] bg-skin-ten p-2 text-white">Reserve Now</Link>
+                            </div>
+                        </div>
+                        <div className="w-full lg:w-1/2 flex lg:justify-end lg:mt-14 min-h-[600px] lg:min-h-0">
+                            <div className="relative size-full aspect-square lg:size-[400px] lg:min-w-[400px] rounded-lg">
+                                <div className="absolute w-[310px] h-[210px] overflow-hidden border-[8px] border-solid border-white rounded-md transform-gpu left-[calc(50%-50px)] top-[calc(50%-80px)] lg:left-[-80px] lg:top-[20px] lg:animate-upright01 rotate-[-20deg] animate-mobileupright01">
+                                    <Image 
+                                        src={ Event01 }
+                                        alt={ 'Paras Sarap Wedding Venue' }
+                                        sizes='100%'
+                                        style={{
+                                            objectFit: 'cover',
+                                            transformOrigin: 'center',
+                                        }}
+                                        priority
+                                    />
+                                </div>
+
+                                <div className="absolute w-[300px] h-[160px] overflow-hidden border-[8px] border-solid border-white rounded-md left-[calc(50%+50px)] top-1/2 lg:left-[60px] lg:top-[140px] lg:translate-x-0 lg:translate-y-0 lg:animate-upright02 animate-mobileupright02">
+                                    <Image 
+                                        src={ Event02 }
+                                        alt={ 'Paras Sarap Debut Venue' }
+                                        sizes='100%'
+                                        style={{
+                                            objectFit: 'cover',
+                                            transformOrigin: 'bottom',
+                                        }}
+                                        priority
+                                    />
+                                </div>
+                                <div className="absolute w-[220px] h-[160px] overflow-hidden border-[8px] border-solid border-white rounded-md transform-gpu left-[calc(50%-80px)] top-[calc(50%+100px)] lg:left-[-20px] lg:top-[240px] lg:translate-x-0 lg:translate-y-0 lg:animate-upright03 animate-mobileupright03">
+                                    <Image 
+                                        src={ Event03 }
+                                        alt={ 'Paras Sarap Christening' }                                    
+                                        sizes='100%'
+                                        style={{
+                                            objectFit: 'cover',
+                                            transformOrigin: 'center',
+                                        }}
+                                        priority
+                                    />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </section>
-                <section className="w-full flex flex-col items-center gap-4 md:h-[calc(100vh-var(--nav-height)-32px)]">
+                <section className="w-full flex flex-col items-center gap-4 md:h-[calc(100vh-var(--nav-height))]">
                     <h3 className="font-headings w-full text-center text-lg font-semibold">About Us</h3>
                     <p className="font-paragraphs text-center md:w-1/2">Paras Sarap Catering Services started in 2012 as a small business by Mrs. Glecy Reyes Serrano, offering Filipino and street foods. With a focus on quality and customer service, they quickly gained popularity, expanding to accept bulk orders and deliveries for special occasions. Recognized for their delicious food, they entered the catering industry, becoming a renowned name in General Tinio and nearby areas, including Baguio, Pangasinan, and Tarlac.</p>
                     <Link href="/about" className="rounded-sm border-[1px] border-neutral-600 p-2 sm:mt-[70px]">Learn More</Link>
                 </section>
-                <section className="w-full flex flex-col md:min-h-[calc(100vh-var(--nav-height)-32px)]">
+                <section className="w-full flex flex-col md:min-h-[calc(100vh-var(--nav-height))]">
                     <h3 className="font-headings w-full text-center text-lg font-semibold leading-none">Services</h3>
                     <p className="font-paragraphs w-full text-center text-neutral-700 text-sm">Choose Service</p>
                     <div className="w-full flex justify-center gap-4 mt-4 flex-wrap">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,58 +1,91 @@
 /** @type {import('tailwindcss').Config} */
 
-import { nextui } from '@nextui-org/react';
-
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        headings: ['var(--font-exo)'],
-        paragraphs: ['var(--font-quicksand)'],
-      },
-      padding: {
-        'page-x': 'var(--page-x-padding)',
-      },
-      backgroundColor: {
-        skin: {
-          ten: 'var(--skin-ten)',
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                headings: ['var(--font-exo)'],
+                paragraphs: ['var(--font-quicksand)'],
+            },
+            padding: {
+                'page-x': 'var(--page-x-padding)',
+            },
+            backgroundColor: {
+                skin: {
+                    ten: 'var(--skin-ten)',
+                },
+            },
+            zIndex: {
+                navbar: 'var(--nav-z-index)',
+                subnavbar: 'var(--subnav-z-index)',
+                footer: 'var(--footer-z-index)',
+            },
+            width: {
+                'admin-sidebar': 'var(--admin-sidebar-width)',
+            },
+            height: {
+                'nav-height': 'var(--nav-height)',
+                'nav-item-height': 'var(--nav-item-height)',
+            },
+            data: {
+                checked: 'ui~="checked"',
+                active: 'ui~="active"',
+            },
+            transitionDelay: {
+                '2000': '2000'
+            },
+            colors: {
+                accent: {
+                    1: 'hsl(var(--color-accent1) / <alpha-value>)',
+                    2: 'hsl(var(--color-accent2) / <alpha-value>)'
+                },
+                background: 'hsl(var(--color-background) / <alpha-value>)',
+                content: 'hsl(var(--color-content) / <alpha-value>)',
+            },
+
+            keyframes: {
+                upright01: {
+                    '0%, 100%': { transform: 'translate(0, 0) rotate(-20deg)' },
+                    '50%': { transform: 'translate(-6px, 6px) rotate(-20deg)' },
+                },
+                upright02: {
+                    '0%, 100%': { transform: 'translate(0, 0) rotate(0deg)' },
+                    '50%': { transform: 'translate(-10px, 10px) rotate(0deg)' },
+                },
+                upright03: {
+                    '0%, 100%': { transform: 'translate(0, 0) rotate(10deg)' },
+                    '50%': { transform: 'translate(-8px, 8px) rotate(10deg)' },
+                },
+
+                mobileupright01: {
+                    '0%, 100%': { transform: 'translate(-50%, -50%) rotate(-20deg)' },
+                    '50%': { transform: 'translate(-46%, -54%) rotate(-20deg)' },
+                },
+                mobileupright02: {
+                    '0%, 100%': { transform: 'translate(-50%, -50%) rotate(0deg)' },
+                    '50%': { transform: 'translate(-44%, -56%) rotate(0deg)' },
+                },
+                mobileupright03: {
+                    '0%, 100%': { transform: 'translate(-50%, -50%) rotate(10deg)' },
+                    '50%': { transform: 'translate(-42%, -58%) rotate(10deg)' },
+                }
+            },
+            animation: {
+                upright01: 'upright01 3s ease-in-out infinite',
+                upright02: 'upright02 4s ease-in-out infinite',
+                upright03: 'upright03 4.6s ease-in-out infinite',
+                    
+                mobileupright01: 'mobileupright01 3s ease-in-out infinite',
+                mobileupright02: 'mobileupright02 4s ease-in-out infinite',
+                mobileupright03: 'mobileupright03 4.6s ease-in-out infinite',
+            },         
         },
-      },
-      zIndex: {
-        navbar: 'var(--nav-z-index)',
-        subnavbar: 'var(--subnav-z-index)',
-        footer: 'var(--footer-z-index)',
-      },
-      width: {
-        'admin-sidebar': 'var(--admin-sidebar-width)',
-      },
-      height: {
-        'nav-height': 'var(--nav-height)',
-        'nav-item-height': 'var(--nav-item-height)',
-      },
-      data: {
-        checked: 'ui~="checked"',
-        active: 'ui~="active"',
-      },
-      transitionDelay: {
-        '2000': '2000'
-      },
-      colors: {
-        accent: {
-          1: 'hsl(var(--color-accent1) / <alpha-value>)',
-          2: 'hsl(var(--color-accent2) / <alpha-value>)'
-        },
-        background: 'hsl(var(--color-background) / <alpha-value>)',
-        content: 'hsl(var(--color-content) / <alpha-value>)',
-      }
     },
-  },
-  // darkMode: "class",
-  // plugins: [nextui()],
-  plugins: [],
+    plugins: [],
 };
+
