d7bff325601c0f005ba709b2a4fd2afac65e501c
lerry <johnlerryramossamson@gmail.com>
2024-07-24 23:47:13 +0800

    Change navigation link's urls for breadcrumbs

--- src/components/client/nav/CSidebar.jsx
+++ src/components/client/nav/CSidebar.jsx
@@ -8,10 +8,10 @@ const CSidebar = () => {
             <Option href="/about" className="!rounded-full px-4">About</Option>
             <Option href="/reserve?display=myreservations" className="!rounded-full px-4">Reservations</Option>
             <ParentOption text="Services" className="px-4">
-                <Option href="/reserve?display=themes&service=wedding" className="!rounded-full px-4">Wedding</Option>
-                <Option href="/reserve?display=themes&service=debut" className="!rounded-full px-4">Debut</Option>
-                <Option href="/reserve?display=themes&service=kidsparty" className="!rounded-full px-4">Kids Party</Option>
-                <Option href="/reserve?display=themes&service=privateparty" className="!rounded-full px-4">Private Party</Option>
+                <Option href="/reserve?display=themes&service=wedding&set=1&series=1" className="!rounded-full px-4">Wedding</Option>
+                <Option href="/reserve?display=themes&service=debut&set=1&series=1" className="!rounded-full px-4">Debut</Option>
+                <Option href="/reserve?display=themes&service=kidsparty&set=1&series=1" className="!rounded-full px-4">Kids Party</Option>
+                <Option href="/reserve?display=themes&service=privateparty&set=1&series=1" className="!rounded-full px-4">Private Party</Option>
             </ParentOption>
             <ParentOption text="Packages" className="px-4">
                 <Option href="" className="!rounded-full px-4">Wedding</Option>
