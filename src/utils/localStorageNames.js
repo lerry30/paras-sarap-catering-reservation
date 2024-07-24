753651ba29ef7b463ed5f49536e68caec65131ea
lerry <johnlerryramossamson@gmail.com>
2024-07-24 23:49:53 +0800

    I added a new slot to localstorage named reservation-cache

--- src/utils/localStorageNames.js
+++ src/utils/localStorageNames.js
@@ -4,5 +4,6 @@ const menu = 'menu-update-data';
 const venue = 'venue-update-data';
 const user = 'user-data';
 const reservation = 'user-reservation';
+const reservationcache = 'reservation-cache';
 
-export { dish, drink, menu, venue, user, reservation };
+export { dish, drink, menu, venue, user, reservation, reservationcache };
