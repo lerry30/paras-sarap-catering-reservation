6c0d48714c52be05adbf60be26b6d16f54917729
lerry <johnlerryramossamson@gmail.com>
2024-07-26 21:26:58 +0800

    I included a way to define some settings for reservation in this case it specify the time limited service

---
+++ src/app/api/policies/reservation/servicetime/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import Policy from '@/models/Policies';
+
+export const GET = async () => {
+    try {
+        const policyData = await Policy.findOne({}); // just get the first one
+        const durationOfServiceInHours = policyData?.timelimitedserviceinhours;
+        const data = { durationOfServiceInHours };
+        return NextResponse.json({ message: '', data }, { status: 200 });
+    } catch(error) {
+        console.log('Fetch limited time for service error: \n', error);
+        return NextResponse.json({ message: 'There\'s something wrong.' }, { status: 400 });
+    }
+}
