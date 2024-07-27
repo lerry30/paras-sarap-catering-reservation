93513c26ad00442cf45a054604c18e715a2049e7
lerry <johnlerryramossamson@gmail.com>
2024-07-27 11:53:18 +0800

    I added a new setting to define how much per hour of service that user wish to

--- src/app/api/policies/reservation/servicetime/route.js
+++ src/app/api/policies/reservation/servicetime/route.js
@@ -4,8 +4,9 @@ import Policy from '@/models/Policies';
 export const GET = async () => {
     try {
         const policyData = await Policy.findOne({}); // just get the first one
-        const durationOfServiceInHours = policyData?.timelimitedserviceinhours;
-        const data = { durationOfServiceInHours };
+        const durationOfServiceInHours = policyData?.timelimitedserviceinhours || 4;
+        const additionalServiceTimeCostPerHour = policyData?.additionalServiceTimeCostPerHour || 1000;
+        const data = { durationOfServiceInHours, additionalServiceTimeCostPerHour };
         return NextResponse.json({ message: '', data }, { status: 200 });
     } catch(error) {
         console.log('Fetch limited time for service error: \n', error);
