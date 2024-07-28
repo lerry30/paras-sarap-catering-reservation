7bcdb0e6561846c75c0574f9f27afb46d8fd1ee2
lerry <johnlerryramossamson@gmail.com>
2024-07-28 23:46:46 +0800

    Ratings model added

---
+++ src/models/Ratings.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose';
+import connectToDatabase from '@/utils/databaseConnection';
+
+mongoose.Promise = global.Promise;
+connectToDatabase();
+
+const ratingSchema = new Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+
+    reservationId: {
+        type: String,
+        required: true
+    },
+
+    point: {
+        type: Number,
+        required: true
+    },
+
+    message: String
+}, {
+    timestamps: true
+});
+
+const Rating = mongoose.models.Rating || mongoose.model('Rating', ratingSchema);
+export default Rating;
