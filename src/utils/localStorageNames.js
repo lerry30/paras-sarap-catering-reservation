1295b3f2efabf5c09355afab00857b814fbd0ad6
lerry <johnlerryramossamson@gmail.com>
2024-07-28 23:44:38 +0800

    New localstorage names added for service rating

--- src/utils/localStorageNames.js
+++ src/utils/localStorageNames.js
@@ -5,5 +5,7 @@ const venue = 'venue-update-data';
 const user = 'user-data';
 const reservation = 'user-reservation';
 const reservationcache = 'reservation-cache';
+const viewedreservationapproved = 'no-of-viewed-reservation-approved';
+const viewedreservationrejected = 'no-of-viewed-reservation-rejected';
 
-export { dish, drink, menu, venue, user, reservation, reservationcache };
+export { dish, drink, menu, venue, user, reservation, reservationcache, viewedreservationapproved, viewedreservationrejected };
