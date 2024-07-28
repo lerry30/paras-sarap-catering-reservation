960a1fa091f8df2a2c90e58d24c4648a0710e4af
lerry <johnlerryramossamson@gmail.com>
2024-07-28 23:36:43 +0800

    Service rating modal added

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,5 @@
-import { X } from '@/components/icons/All';
-import { useRef, useState } from 'react';
+import { X, Star } from '@/components/icons/All';
+import { useRef, useState, useLayoutEffect } from 'react';
 import ErrorField from './ErrorField';
 
 export const SuccessModal = ({ message, callback }) => {
@@ -38,7 +38,7 @@ export const ErrorModal = ({ header, message, callback }) => {
     }
 
     return (
-        <div ref={ modalRef } className="w-screen h-screen fixed top-0 bg-neutral-800/90 z-50 flex justify-center items-center">
+        <div ref={ modalRef } className="w-screen h-screen fixed top-0 left-0 bg-neutral-800/90 z-50 flex justify-center items-center">
             <div className="card p-16 bg-zinc-100 rounded-md shadow-lg shadow-black border border-rose-700 flex flex-col">
                 <article className="w-full relative">
                     <div onClick={ closeModal } className="group absolute top-[-20px] right-[-20px]">
@@ -87,7 +87,7 @@ export const Prompt = ({ header, message, callback, onClose }) => {
     );
 }
 
-export const PromptTextBox = ({ header, message, callback }) => {
+export const PromptTextBox = ({ header, message, callback, onClose }) => {
     const modalRef = useRef();
     const [ textBoxError, setTextBoxError ] = useState('');
 
@@ -112,6 +112,11 @@ export const PromptTextBox = ({ header, message, callback }) => {
             setTextBoxError(error?.message || 'There\'s something wrong.');
         }
     }
+    
+    const closeModal = () => { 
+        modalRef.current.style.display = 'none';
+        onClose();
+    }
 
     return (
         <div ref={ modalRef } className="w-screen h-screen fixed top-0 left-0 bg-neutral-800/90 z-50 flex justify-center items-center backdrop-blur-sm">
@@ -124,6 +129,7 @@ export const PromptTextBox = ({ header, message, callback }) => {
                     <ErrorField message={ textBoxError } />
 
                     <div className="w-full justify-end flex gap-2 mt-2">
+                        <button onClick={ closeModal } className="font-headings bg-neutral-500/45 p-2 leading-none rounded-full text-[16px]">Cancel</button>
                         <button type="submit" className="font-headings bg-neutral-500/45 px-4 py-1 leading-none rounded-full text-[16px]">Submit</button>
                     </div>
                 </form>
@@ -190,6 +196,88 @@ export const PromptAgreement = ({ callback, onClose, deadline }) => {
                 <div className="w-full justify-end flex gap-2 mt-2">
                     <button onClick={ agree } className={ `font-headings bg-neutral-500/45 px-4 py-1 leading-none rounded-full text-[16px] ${ agreed || 'opacity-70' }` }>Yes</button>
                     <button onClick={ closeModal } className="font-headings bg-neutral-500/45 p-2 leading-none rounded-full text-[16px]">Cancel</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export const PromptRating = ({ callback, onClose }) => {
+    const modalRef = useRef();
+    const starWidth = useRef(50);
+
+    const [ point, setPoint ] = useState(0);
+    const [ shade, setShade ] = useState(Array(5).fill(0));
+
+    const closeModal = () => {
+        modalRef.current.style.display = 'none';
+        onClose();
+    }
+
+    const rateInStar = (ev, number) => {
+        const rectProps = ev.target.getBoundingClientRect();
+        const mouseX = ev.clientX;
+
+        const xDistance = Math.ceil(mouseX - rectProps?.left);
+        const rate = xDistance / starWidth.current + number;
+        setPoint(Number(rate.toFixed(1)));
+        setShade(nShade => {
+            for(let i = 0; i < nShade.length; i++) {
+                nShade[i] = 0;
+                if(i < number) nShade[i] = starWidth.current;
+            }
+
+            nShade[number] = xDistance; 
+            return nShade; 
+        });
+    }
+
+    useLayoutEffect(() => {
+        if(innerWidth < 600)
+            starWidth.current = 40;
+    }, []);
+
+    return (
+        <div ref={ modalRef } className="w-screen h-screen fixed top-0 left-0 bg-neutral-800/90 z-50 flex justify-center items-center backdrop-blur-sm">
+            <div className="card p-16 bg-zinc-100 rounded-md shadow-lg shadow-black border border-emerald-500 flex flex-col">
+                <article className="w-full relative">
+                    <div onClick={ closeModal } className="group absolute top-[-20px] right-[-20px]">
+                        <X
+                            onClick={ closeModal }
+                            className="cursor-pointer rounded-full group-hover:bg-neutral-800/50 group-hover:stroke-neutral-200 stroke-neutral-500/75" 
+                        />
+                    </div>
+                </article>
+ 
+                <h1 className="font-headings font-bold text-2xl pt-2 max-w-80 text-center">Service Satisfaction Rating</h1>
+                <p className="font-paragraphs w-60 py-2 text-center text-neutral-800">Please rate your overall service satisfaction from 1 to 5.</p>
+
+                <h2 className="font-headings font-semibold text-2xl py-2">{ !!point && point }</h2>
+                <div className="flex gap-[4px]"
+                    style={{ 
+                        width: `${starWidth.current*5+16}px`, 
+                        height: `${starWidth.current}px`,
+                }}>
+                    {
+                        shade.map((nShade, index) => (
+                            <div key={ index } className="relative"
+                                style={{
+                                    width: `${starWidth.current}px`,
+                                    height: `${starWidth.current}px`
+                                }}
+                                onClick={ (ev) => rateInStar(ev, index)
+                            }>
+                                <Star size={ starWidth.current } className="absolute top-0 left-0 right-0 bottom-0" strokeWidth={1} />
+                                <div className="overflow-hidden absolute top-0 left-0 right-0 bottom-0" style={{ width: `${ nShade }px` }}>
+                                    <Star size={ starWidth.current } className="fill-yellow-400" strokeWidth={1} />
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="w-full justify-end flex gap-2 mt-6">
+                    <button onClick={ closeModal } className="font-headings bg-neutral-500/45 p-2 leading-none rounded-full text-[16px]">Cancel</button>
+                    <button onClick={ () => callback(point) } className="font-headings bg-neutral-500/45 px-4 py-1 leading-none rounded-full text-[16px]">Submit</button>
                 </div>
             </div>
         </div>
