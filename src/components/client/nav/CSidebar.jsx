8338af3af8c108b0dbf25d7625c121646586c628
lerry <johnlerryramossamson@gmail.com>
2024-08-17 08:55:12 +0800

    I just added the links for FAQs as well as rearranged it properly

--- src/components/client/nav/CSidebar.jsx
+++ src/components/client/nav/CSidebar.jsx
@@ -7,6 +7,8 @@ const CSidebar = () => {
         <BurgerMenu size="h-nav-item-height" className="mr-1 lg:hidden">
             <Option href="/about" className="!rounded-full px-4">About</Option>
             <Option href="/reserve?display=myreservations" className="!rounded-full px-4">Reservations</Option>
+            <Option href="/faqs" className="!rounded-full px-4">FAQs</Option>
+            <Option href="/contact" className="!rounded-full px-4">Contact</Option>
             <ParentOption text="Services" className="px-4">
                 <Option href="/reserve?display=themes&service=wedding&set=1&series=1" className="!rounded-full px-4">Wedding</Option>
                 <Option href="/reserve?display=themes&service=debut&set=1&series=1" className="!rounded-full px-4">Debut</Option>
@@ -19,7 +21,6 @@ const CSidebar = () => {
                 <Option href="" className="!rounded-full px-4">Kids Party</Option>
                 <Option href="" className="!rounded-full px-4">Private Party</Option>
             </ParentOption> */ }
-            <Option href="/contact" className="!rounded-full px-4">Contact</Option>
         </BurgerMenu>
     );
 }
